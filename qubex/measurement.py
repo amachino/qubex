fc98787f5d4300c6dafb65234183ebdb8c3d71b8
Akinori Machino <@amachino>
2023-10-08 21:12:26 +0900

    Remove params.py

--- qubex/measurement.py
+++ qubex/measurement.py
@@ -4,22 +4,18 @@ A module to represent an measurement.
 
 import os
 from datetime import datetime
-from typing import Any
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 
-from qubecalib import Qube
+import qubecalib as qc
 from qubecalib.qube import SSB
 from qubecalib.pulse import Schedule, Channel, Arbitrary, Blank, Read
 from qubecalib.setupqube import run
 
 from .pulse import Waveform, Rcft
-from .params import ctrl_freq_dict, ro_freq_dict, ro_ampl_dict
 
-QUBE_ID = "riken_1-07"
-QUBITS = ["Q08", "Q09", "Q10", "Q11"]
 CTRL = "CTRL_"
 READ_TX = "READ_TX_"
 READ_RX = "READ_RX_"
@@ -27,9 +23,6 @@ T_CONTROL = 10 * 2048  # maximum duration of control [ns]
 T_READOUT = 128 * 12  # maximum duration of readout [ns]
 T_MARGIN = 128 * 2  # margin between readout and control [ns]
 READ_SLICE_RANGE = slice(200, int(200 + 800 / 1.5))
-CONTROL_FREQUENCY = ctrl_freq_dict
-READOUT_FREQUENCY = ro_freq_dict[QUBE_ID]
-READOUT_AMPLITUDE = ro_ampl_dict[QUBE_ID]
 
 
 MeasuredState = dict[str, complex]
@@ -40,9 +33,9 @@ class Measurement:
     A class to represent an measurement.
     """
 
-    def __init__(self, qube_id: str = QUBE_ID):
-        self.qubits = QUBITS
+    def __init__(self, qube_id: str, qubits: dict):
         self.qube = self.init_qube(qube_id=qube_id)
+        self.qubits = qubits
         self.schedule = Schedule()
         self.init_channels()
         self.init_schedule()
@@ -52,80 +45,38 @@ class Measurement:
         """
         Initialize the QuBE.
         """
-        qube: Any = Qube.create(f"qube_{qube_id}.yml")
+        # qube: Any = Qube.create(f"qube_{qube_id}.yml")
+        qc.ui.MATPLOTLIB_PYPLOT = plt
+        qube = qc.ui.QubeControl(f"qube_{qube_id}.yml").qube
 
         if qube is None:
             raise ValueError(f"QuBE {qube_id} is not found.")
 
-        # mix.vatt: Attenuator value (max: 0xFFF, min: 0x000)
-        # The value saturates around 0xC00, so set it to 0xC00 if you want to get the maximum value.
-        # Since the noise may be high at 0xC00, set it to 0x800 for channels except for CR pulses.
-
-        # pylint: disable=no-member
-
-        # Readout TX
-        qube.port0.lo.mhz = 12500
-        qube.port0.nco.mhz = 2500
         qube.port0.mix.ssb = SSB.LSB
-        qube.port0.awg0.nco.mhz = 0
-        qube.port0.mix.vatt = 0x800
-
-        # Reatout RX
-        qube.port1.nco.mhz = 2500
         qube.port1.adc.capt0.ssb = SSB.LSB
-        qube.port1.delay = 128 + 6 * 128  # [ns]
-
-        # Control Q08
-        qube.port5.lo.mhz = 10000
-        qube.port5.nco.mhz = 2000
-        qube.port5.awg0.nco.mhz = 375
-        qube.port5.awg1.nco.mhz = 0  # NG
-        qube.port5.awg2.nco.mhz = 0
-        qube.port5.mix.vatt = 0xC00
-
-        # Control Q09
-        qube.port6.lo.mhz = 11000
-        qube.port6.nco.mhz = 2250
-        qube.port6.awg0.nco.mhz = 375
-        qube.port6.awg1.nco.mhz = 0
-        qube.port6.awg2.nco.mhz = 375
-        qube.port6.mix.vatt = 0x800
-
-        # Control Q10
-        qube.port7.lo.mhz = 11000
-        qube.port7.nco.mhz = 2500
-        qube.port7.awg0.nco.mhz = 375
-        qube.port7.awg1.nco.mhz = 0
-        qube.port7.awg2.nco.mhz = -375
-        qube.port7.mix.vatt = 0x800
-
-        # Control Q11
-        qube.port8.lo.mhz = 10000
-        qube.port8.nco.mhz = 2250
-        qube.port8.awg0.nco.mhz = 375
-        qube.port8.awg1.nco.mhz = 0
-        qube.port8.awg2.nco.mhz = -375
-        qube.port8.mix.vatt = 0xC00
-
-        # pylint: enable=no-member
+        qube.port12.adc.capt0.ssb = SSB.LSB
+        qube.port13.mix.ssb = SSB.LSB
         return qube
 
     def init_channels(self):
         """
         Initialize the channels.
         """
-        for qubit in self.qubits:
+        for qubit, params in self.qubits.items():
             # control channels
             self.set_control_channel(
-                qubit, Channel(center_frequency=CONTROL_FREQUENCY[qubit])
+                qubit,
+                Channel(center_frequency=params["freq_ge"]),
             )
             # readout (tx) channels
             self.set_readout_tx_channel(
-                qubit, Channel(center_frequency=READOUT_FREQUENCY[qubit])
+                qubit,
+                Channel(center_frequency=params["freq_ro"]),
             )
             # readout (rx) channels
             self.set_readout_rx_channel(
-                qubit, Channel(center_frequency=READOUT_FREQUENCY[qubit])
+                qubit,
+                Channel(center_frequency=params["freq_ro"]),
             )
 
     def init_schedule(self):
@@ -160,34 +111,40 @@ class Measurement:
         """
         Initialize the readout pulses.
         """
-        for qubit in self.qubits:
+        for qubit, params in self.qubits.items():
             # readout (tx) pulses
-            # NOTE: Check the parameters
             pulse = Rcft(
-                ampl=READOUT_AMPLITUDE[qubit],
+                ampl=params["ampl_ro"],
                 rise=50,
                 flat=int(T_READOUT / 1.5),
                 fall=50,
             )
             self.set_readout_waveform(qubit, pulse)
 
-    def measure(self, repeats=10_000, interval=100_000) -> MeasuredState:
+    def measure(
+        self,
+        readout_ports: tuple[str, str] = ("port0", "port1"),
+        repeats=10_000,
+        interval=100_000,
+    ) -> MeasuredState:
         """
         Runs the measurement.
         """
-        read_tx_channels = [self.readout_tx_channel(qubit) for qubit in self.qubits]
-        read_rx_channels = [self.readout_rx_channel(qubit) for qubit in self.qubits]
+        tx_port = self.qube.ports[readout_ports[0]]
+        rx_port = self.qube.ports[readout_ports[1]]
+        tx_channels = [self.readout_tx_channel(qubit) for qubit in self.qubits]
+        rx_channels = [self.readout_rx_channel(qubit) for qubit in self.qubits]
 
-        adda_to_channels = {  # NOTE: hi/lo settings
-            self.qube.port0.dac.awg0: read_tx_channels,
-            self.qube.port1.adc.capt0: read_rx_channels,
-            self.qube.port5.dac.awg2: [self.control_channel("Q08")],  # NOTE: AWG1 is NG
-            self.qube.port6.dac.awg1: [self.control_channel("Q09")],
-            self.qube.port7.dac.awg1: [self.control_channel("Q10")],
-            self.qube.port8.dac.awg1: [self.control_channel("Q11")],
+        adda_to_channels = {
+            tx_port.dac.awg0: tx_channels,
+            rx_port.adc.capt0: rx_channels,
         }
 
-        triggers = [self.qube.port0.dac.awg0]
+        for qubit, param in self.qubits.items():
+            port = self.qube.ports[param["port"]]
+            adda_to_channels[port.dac.awg1] = [self.control_channel(qubit)]
+
+        triggers = [tx_port.dac.awg0]
 
         run(
             self.schedule,
@@ -209,7 +166,7 @@ class Measurement:
             waveform = self.readout_rx_waveform(qubit)
             state[qubit] = waveform.iq[READ_SLICE_RANGE].mean()
             # save the readout data as a file
-            self.save_readout_data(qubit, waveform)
+            # self.save_readout_data(qubit, waveform)
         return state
 
     def save_readout_data(self, qubit: str, waveform: Waveform):
@@ -331,7 +288,7 @@ class Measurement:
 
         # initialize the axes
         # share the x-axis with the first subplot
-        axes: list[plt.Axes] = []
+        axes: list = []
         for i, qubit in enumerate(self.qubits):
             if i == 0:
                 ax = plt.subplot(gs[i])
--- qubex/pulse.py
+++ qubex/pulse.py
@@ -3,6 +3,8 @@ A module for representing pulses.
 """
 import numpy as np
 
+from tabuchi_dd.pulse import TabuchiPulse
+
 from .waveform import Waveform
 from .waveform import SAMPLING_TIME
 
@@ -40,35 +42,39 @@ class Rcft(Waveform):
         super().__init__(iq)
 
 
-# class Drag(Waveform):
-#     """
-#     A Derivative Removal by Adiabatic Gate (DRAG) pulse.
-#     """
-
-#     def __init__(self, duration=30, ampl=1.0, beta=1.0):
-#         """
-#         Initializes a new DRAG pulse with given parameters.
-
-#         Parameters
-#         ----------
-#         duration : int
-#             Duration of the pulse in ns.
-#         ampl : float
-#             Peak amplitude of the pulse.
-#         beta : float
-#             DRAG coefficient that characterizes the amplitude of the correction term.
-#         """
-#         t = np.arange(0, duration, SAMPLING_TIME)
-#         sigma = duration * 0.5
-#         envelope = ampl * (np.exp(-0.5 * (t / sigma) ** 2) - np.exp(0.5))
-#         correction = beta * (-t / (sigma**2)) * np.exp(-0.5 * (t / sigma) ** 2)
-
-#         iq = envelope + 1j * correction
-
-#         super().__init__(iq)
-
-
 class Drag(Waveform):
+    """
+    A Derivative Removal by Adiabatic Gate (DRAG) pulse.
+    """
+
+    def __init__(self, duration=30, ampl=1.0, beta=1.0):
+        """
+        Initializes a new DRAG pulse with given parameters.
+
+        Parameters
+        ----------
+        duration : int
+            Duration of the pulse in ns.
+        ampl : float
+            Peak amplitude of the pulse.
+        beta : float
+            DRAG coefficient that characterizes the amplitude of the correction term.
+        """
+        t = np.arange(0, duration, SAMPLING_TIME)
+        sigma = duration * 0.5
+        envelope = (
+            ampl
+            * (np.exp(-((t - sigma) ** 2) / (2 * sigma**2)) - np.exp(-0.5))
+            / (1 - np.exp(-0.5))
+        )
+        correction = -beta * (t - sigma) / sigma * envelope
+
+        iq = envelope + 1j * correction
+
+        super().__init__(iq)
+
+
+class DragCos(Waveform):
     """
     A Derivative Removal by Adiabatic Gate (DRAG) pulse.
     """
@@ -95,44 +101,54 @@ class Drag(Waveform):
         super().__init__(iq)
 
 
-class QctrlPi(Waveform):
+# class QctrlPi(Waveform):
+#     """
+#     An optimized pi pulse.
+#     """
+
+#     def __init__(self):
+#         iq = np.array(
+#             [
+#                 0.03481618 + 5.94384947e-03j,
+#                 0.03265834 + 9.58042570e-03j,
+#                 0.02718015 + 4.56709459e-03j,
+#                 0.03008459 + 1.07180817e-02j,
+#                 -0.00023517 + 1.06311655e-04j,
+#                 0.02231457 + 1.06699124e-03j,
+#                 0.02500387 + 9.39997498e-03j,
+#                 -0.00029007 - 1.53053753e-04j,
+#                 0.03373711 - 3.31980694e-03j,
+#                 0.02424721 + 1.32870650e-04j,
+#                 0.00661651 + 5.76160314e-03j,
+#                 0.01289346 + 3.03768648e-03j,
+#                 0.02347185 + 5.50176897e-03j,
+#                 0.03545285 + 2.31146867e-02j,
+#                 0.02416318 - 6.78417157e-04j,
+#                 0.03165002 + 2.80328007e-03j,
+#                 0.00455699 + 1.62375270e-02j,
+#                 0.02475384 - 1.86465684e-03j,
+#                 0.00508424 - 4.86456888e-03j,
+#                 0.01028951 - 7.75430524e-03j,
+#                 0.01918622 - 1.04645872e-02j,
+#                 0.0099093 - 4.08165474e-03j,
+#                 0.03882687 + 2.27914374e-02j,
+#                 0.0283639 + 2.94183055e-02j,
+#                 0.0152782 + 6.45479207e-03j,
+#                 -0.01709611 - 8.82758026e-03j,
+#                 0.04212434 + 1.40447913e-02j,
+#                 0.04487165 - 3.15664494e-03j,
+#                 -0.00468664 + 6.60658952e-03j,
+#                 0.0131243 + 5.73832368e-05j,
+#             ]
+#         )
+#         super().__init__(iq)
+
+
+class TabuchiDD(Waveform):
     """
-    An optimized pi pulse.
+    Tabuchi DD pulse.
     """
 
-    def __init__(self):
-        iq = np.array(
-            [
-                0.03481618 + 5.94384947e-03j,
-                0.03265834 + 9.58042570e-03j,
-                0.02718015 + 4.56709459e-03j,
-                0.03008459 + 1.07180817e-02j,
-                -0.00023517 + 1.06311655e-04j,
-                0.02231457 + 1.06699124e-03j,
-                0.02500387 + 9.39997498e-03j,
-                -0.00029007 - 1.53053753e-04j,
-                0.03373711 - 3.31980694e-03j,
-                0.02424721 + 1.32870650e-04j,
-                0.00661651 + 5.76160314e-03j,
-                0.01289346 + 3.03768648e-03j,
-                0.02347185 + 5.50176897e-03j,
-                0.03545285 + 2.31146867e-02j,
-                0.02416318 - 6.78417157e-04j,
-                0.03165002 + 2.80328007e-03j,
-                0.00455699 + 1.62375270e-02j,
-                0.02475384 - 1.86465684e-03j,
-                0.00508424 - 4.86456888e-03j,
-                0.01028951 - 7.75430524e-03j,
-                0.01918622 - 1.04645872e-02j,
-                0.0099093 - 4.08165474e-03j,
-                0.03882687 + 2.27914374e-02j,
-                0.0283639 + 2.94183055e-02j,
-                0.0152782 + 6.45479207e-03j,
-                -0.01709611 - 8.82758026e-03j,
-                0.04212434 + 1.40447913e-02j,
-                0.04487165 - 3.15664494e-03j,
-                -0.00468664 + 6.60658952e-03j,
-                0.0131243 + 5.73832368e-05j,
-            ]
-        )
+    def __init__(self, duration, step):
+        iq = TabuchiPulse(duration=duration, step=step).values
         super().__init__(iq)
