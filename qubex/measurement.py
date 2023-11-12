a26df4753379aa3695947a0b18a1719024fe9ccb
Akinori Machino <@amachino>
2023-11-12 19:53:06 +0900

    Add show_pulse_sequences and show_measurement_results to plot module

--- qubex/measurement.py
+++ qubex/measurement.py
@@ -5,7 +5,6 @@ from dataclasses import dataclass
 import numpy as np
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 from IPython.display import clear_output
 
 import qubecalib as qc
@@ -16,6 +15,7 @@ qc.ui.MATPLOTLIB_PYPLOT = plt  # type: ignore
 
 from .pulse import Rect, Waveform, PulseSequence
 from .analysis import rotate, get_angle, fit_rabi
+from .plot import show_pulse_sequences, show_measurement_results
 from .typing import (
     QubitKey,
     QubitDict,
@@ -507,222 +507,26 @@ class Measurement:
         readout_rx_times = self.get_readout_rx_times(readout_qubits)
 
         clear_output(True)
-        self.show_measurement_results(
+        show_measurement_results(
             readout_qubits=readout_qubits,
             readout_rx_waveforms=readout_rx_waveforms,
             readout_rx_times=readout_rx_times,
             sweep_range=sweep_range[: idx + 1],
             signals=signals,
             signals_rotated=signals_rotated,
+            readout_range=READOUT_RANGE,
         )
-        self.show_pulse_sequences(
+        show_pulse_sequences(
             control_qubits=control_qubits,
             control_waveforms=control_waveforms,
             control_times=control_times,
+            control_duration=self.ctrl_duration_,
             readout_tx_qubits=readout_qubits,
             readout_tx_waveforms=readout_tx_waveforms,
             readout_tx_times=readout_tx_times,
+            readout_tx_duration=T_READOUT,
         )
         print(f"{idx+1}/{len(sweep_range)}")
-
-    def show_pulse_sequences(
-        self,
-        control_qubits: list[QubitKey],
-        control_waveforms: QubitDict[IQArray],
-        control_times: QubitDict[IntArray],
-        readout_tx_qubits: list[QubitKey],
-        readout_tx_waveforms: QubitDict[IQArray],
-        readout_tx_times: QubitDict[IntArray],
-    ):
-        """
-        Shows the pulse sequences.
-        """
-
-        # number of qubits
-        N = len(control_qubits)
-
-        # initialize the figure
-        plt.figure(figsize=(15, 1.5 * (N + 1)))
-        plt.subplots_adjust(hspace=0.0)  # no space between subplots
-
-        # the grid is adjusted to the number of qubits (N)
-        # the last row (N+1) is for the readout waveform
-        gs = gridspec.GridSpec(N + 1, 1)
-
-        # the x-axis range
-        xlim = (
-            min(-1.0, -self.ctrl_duration_ * 1e-3),
-            T_READOUT * 1e-3,
-        )
-
-        # initialize the axes
-        # share the x-axis with the first subplot
-        axes = []
-        for i in range(N):
-            if i == 0:
-                ax = plt.subplot(gs[i])
-                ax.set_title("Pulse waveform")
-                ax.set_xlim(xlim)  # μs
-                ax.xaxis.set_visible(False)
-                axes.append(ax)
-            else:
-                ax = plt.subplot(gs[i], sharex=axes[0])
-                ax.xaxis.set_visible(False)
-                axes.append(ax)
-        ro_ax = plt.subplot(gs[N], sharex=axes[0])
-        ro_ax.set_xlabel("Time (μs)")
-        ro_ax.xaxis.set_visible(True)
-        axes.append(ro_ax)
-
-        # the list of the maximum amplitudes used for the ylim
-        max_ampl_list = []
-
-        # plot the control pulses
-        # the real and imaginary parts are plotted in the same subplot
-        for i, qubit in enumerate(control_qubits):
-            times = control_times[qubit] * 1e-3  # ns -> μs
-            waveform = control_waveforms[qubit]
-            axes[i].plot(
-                times,
-                np.real(waveform),
-                label=qubit + " control (real)",
-            )
-            axes[i].plot(
-                times,
-                np.imag(waveform),
-                label=qubit + " control (imag)",
-            )
-            axes[i].legend()
-            max_ampl_list.append(np.max(np.abs(waveform)))
-
-        # plot the readout pulses
-        for i, qubit in enumerate(readout_tx_qubits):
-            times = readout_tx_times[qubit] * 1e-3  # ns -> us
-            waveform = readout_tx_waveforms[qubit]
-            axes[N].plot(
-                times,
-                np.abs(waveform),
-                label=qubit + " readout (abs)",
-                linestyle="dashed",
-            )
-            axes[N].legend()
-            max_ampl_list.append(np.max(np.abs(waveform)))
-
-        # set the y-axis range according to the maximum amplitude
-        max_ampl = np.max(max_ampl_list)
-        for i in range(N + 1):
-            axes[i].set_ylim(-1.1 * max_ampl, 1.1 * max_ampl)
-
-        plt.show()
-
-    def show_measurement_results(
-        self,
-        readout_qubits: list[QubitKey],
-        readout_rx_waveforms: QubitDict[IQArray],
-        readout_rx_times: QubitDict[IntArray],
-        sweep_range: NDArray,
-        signals: QubitDict[list[IQValue]],
-        signals_rotated: QubitDict[IQArray],
-    ):
-        plt.figure(figsize=(15, 6 * len(readout_qubits)))
-        gs = gridspec.GridSpec(2 * len(readout_qubits), 2, wspace=0.3, hspace=0.5)
-
-        ax = {}
-        for i, qubit in enumerate(readout_qubits):
-            ax[qubit] = [
-                plt.subplot(gs[i * 2, 0]),
-                plt.subplot(gs[i * 2 + 1, 0]),
-                plt.subplot(gs[i * 2 : i * 2 + 2, 1]),
-            ]
-
-        for qubit in readout_qubits:
-            """検波した読み出しパルス波形表示"""
-            avg_num = 50  # 平均化する個数
-
-            mov_avg_readout_iq = (
-                np.convolve(readout_rx_waveforms[qubit], np.ones(avg_num), mode="valid")
-                / avg_num
-            )  # 移動平均
-            mov_avg_readout_iq = np.append(mov_avg_readout_iq, np.zeros(avg_num - 1))
-
-            ax[qubit][0].plot(
-                readout_rx_times[qubit] * 1e-3,
-                np.real(mov_avg_readout_iq),
-                label="I",
-            )
-            ax[qubit][0].plot(
-                readout_rx_times[qubit] * 1e-3,
-                np.imag(mov_avg_readout_iq),
-                label="Q",
-            )
-
-            ax[qubit][0].plot(
-                readout_rx_times[qubit][READOUT_RANGE] * 1e-3,
-                np.real(mov_avg_readout_iq)[READOUT_RANGE],
-                lw=5,
-            )
-            ax[qubit][0].plot(
-                readout_rx_times[qubit][READOUT_RANGE] * 1e-3,
-                np.imag(mov_avg_readout_iq)[READOUT_RANGE],
-                lw=5,
-            )
-
-            ax[qubit][0].set_xlabel("Time (μs)")
-            ax[qubit][0].set_xlim(0, 2.0)
-            ax[qubit][0].set_title("Detected readout pulse waveform " + qubit)
-            ax[qubit][0].legend()
-            ax[qubit][0].grid()
-
-            """Rabi振動"""
-            ax[qubit][1].plot(sweep_range, np.real(signals[qubit]), "o-", label="I")
-            ax[qubit][1].plot(sweep_range, np.imag(signals[qubit]), "o-", label="Q")
-            ax[qubit][1].set_xlabel("Sweep index")
-            ax[qubit][1].set_title("Detected signal " + qubit)
-            ax[qubit][1].legend()
-            ax[qubit][1].grid()
-
-            """IQ平面上での複素振幅"""
-            ax[qubit][2].plot(
-                np.real(mov_avg_readout_iq), np.imag(mov_avg_readout_iq), lw=0.2
-            )
-
-            width = max(np.abs(signals[qubit]))
-            ax[qubit][2].set_xlim(-width, width)
-            ax[qubit][2].set_ylim(-width, width)
-            ax[qubit][2].plot(
-                np.linspace(-width, width, 2), np.zeros(2), linewidth=1, color="black"
-            )
-            ax[qubit][2].plot(
-                np.zeros(2), np.linspace(-width, width, 2), linewidth=1, color="black"
-            )
-            ax[qubit][2].set_xlabel("I")
-            ax[qubit][2].set_ylabel("Q")
-            ax[qubit][2].set_title("Complex amplitude on IQ plane " + qubit)
-
-            ax[qubit][2].scatter(
-                np.real(signals[qubit]),
-                np.imag(signals[qubit]),
-                label="Before rotation",
-            )
-            ax[qubit][2].scatter(
-                np.real(signals[qubit])[0],
-                np.imag(signals[qubit])[0],
-                color="blue",
-            )
-
-            ax[qubit][2].scatter(
-                np.real(signals_rotated[qubit]),
-                np.imag(signals_rotated[qubit]),
-                label="After rotation",
-            )
-            ax[qubit][2].scatter(
-                np.real(signals_rotated[qubit][0]),
-                np.imag(signals_rotated[qubit][0]),
-                color="red",
-            )
-            ax[qubit][2].legend()
-            ax[qubit][2].grid()
-        plt.show()
 
     def fit_rabi_params(
         self,
--- qubex/plot.py
+++ qubex/plot.py
@@ -3,86 +3,216 @@ a plot library for qubex
 """
 
 import numpy as np
-import numpy.typing as npt
+from numpy.typing import NDArray
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
-from .measurement import READOUT_RANGE
-from .pulse import Waveform
-from .analysis import get_angle
+from .typing import (
+    QubitKey,
+    QubitDict,
+    IQValue,
+    IQArray,
+    IntArray,
+)
 
 
-def plot_readout_waveform(qubit: str, waveform: Waveform):
+def show_pulse_sequences(
+    control_qubits: list[QubitKey],
+    control_waveforms: QubitDict[IQArray],
+    control_times: QubitDict[IntArray],
+    control_duration: int,
+    readout_tx_qubits: list[QubitKey],
+    readout_tx_waveforms: QubitDict[IQArray],
+    readout_tx_times: QubitDict[IntArray],
+    readout_tx_duration: int,
+):
     """
-    Plots the readout waveform.
+    Shows the pulse sequences.
     """
-    AVG_NUM = 50
 
-    ax = plt.subplot()
-    ax.set_title("Detected readout waveform " + qubit)
-    ax.set_xlabel("Time / us")
-    ax.set_xlim(0, 2.0)
-    ax.grid()
+    # number of qubits
+    N = len(control_qubits)
 
-    window = np.ones(AVG_NUM)
-    mov_avg = np.convolve(waveform.values, window, mode="valid") / AVG_NUM
-    mov_avg = np.append(mov_avg, np.zeros(AVG_NUM - 1))
-    times = waveform.times * 1e-3
+    # initialize the figure
+    plt.figure(figsize=(15, 1.5 * (N + 1)))
+    plt.subplots_adjust(hspace=0.0)  # no space between subplots
 
-    ax.plot(times, np.real(mov_avg), label="I")
-    ax.plot(times, np.imag(mov_avg), label="Q")
+    # the grid is adjusted to the number of qubits (N)
+    # the last row (N+1) is for the readout waveform
+    gs = gridspec.GridSpec(N + 1, 1)
 
-    sliced_time = times[READOUT_RANGE]
-    ax.axvspan(
-        sliced_time[0],
-        sliced_time[-1],
-        color="gray",
-        alpha=0.1,
+    # the x-axis range
+    xlim = (
+        min(-1.0, -control_duration * 1e-3),
+        readout_tx_duration * 1e-3,
     )
+
+    # initialize the axes
+    # share the x-axis with the first subplot
+    axes = []
+    for i in range(N):
+        if i == 0:
+            ax = plt.subplot(gs[i])
+            ax.set_title("Pulse waveform")
+            ax.set_xlim(xlim)  # μs
+            ax.xaxis.set_visible(False)
+            axes.append(ax)
+        else:
+            ax = plt.subplot(gs[i], sharex=axes[0])
+            ax.xaxis.set_visible(False)
+            axes.append(ax)
+    ro_ax = plt.subplot(gs[N], sharex=axes[0])
+    ro_ax.set_xlabel("Time (μs)")
+    ro_ax.xaxis.set_visible(True)
+    axes.append(ro_ax)
+
+    # the list of the maximum amplitudes used for the ylim
+    max_ampl_list = []
+
+    # plot the control pulses
+    # the real and imaginary parts are plotted in the same subplot
+    for i, qubit in enumerate(control_qubits):
+        times = control_times[qubit] * 1e-3  # ns -> μs
+        waveform = control_waveforms[qubit]
+        axes[i].plot(
+            times,
+            np.real(waveform),
+            label=qubit + " control (real)",
+        )
+        axes[i].plot(
+            times,
+            np.imag(waveform),
+            label=qubit + " control (imag)",
+        )
+        axes[i].legend()
+        max_ampl_list.append(np.max(np.abs(waveform)))
+
+    # plot the readout pulses
+    for i, qubit in enumerate(readout_tx_qubits):
+        times = readout_tx_times[qubit] * 1e-3  # ns -> us
+        waveform = readout_tx_waveforms[qubit]
+        axes[N].plot(
+            times,
+            np.abs(waveform),
+            label=qubit + " readout (abs)",
+            linestyle="dashed",
+        )
+        axes[N].legend()
+        max_ampl_list.append(np.max(np.abs(waveform)))
+
+    # set the y-axis range according to the maximum amplitude
+    max_ampl = np.max(max_ampl_list)
+    for i in range(N + 1):
+        axes[i].set_ylim(-1.1 * max_ampl, 1.1 * max_ampl)
+
     plt.show()
 
 
-def plot_states_before_after_rotation(data: npt.ArrayLike):
-    states = np.array(data)
-    rotated_states, _ = get_angle(data)
+def show_measurement_results(
+    readout_qubits: list[QubitKey],
+    readout_rx_waveforms: QubitDict[IQArray],
+    readout_rx_times: QubitDict[IntArray],
+    sweep_range: NDArray,
+    signals: QubitDict[list[IQValue]],
+    signals_rotated: QubitDict[IQArray],
+    readout_range: slice,
+):
+    plt.figure(figsize=(15, 6 * len(readout_qubits)))
+    gs = gridspec.GridSpec(2 * len(readout_qubits), 2, wspace=0.3, hspace=0.5)
 
-    _, axs = plt.subplots(1, 2, figsize=(8, 4))
+    ax = {}
+    for i, qubit in enumerate(readout_qubits):
+        ax[qubit] = [
+            plt.subplot(gs[i * 2, 0]),
+            plt.subplot(gs[i * 2 + 1, 0]),
+            plt.subplot(gs[i * 2 : i * 2 + 2, 1]),
+        ]
 
-    axs[0].scatter(states.real, states.imag)
-    axs[0].set_title("Before Rotation")
+    for qubit in readout_qubits:
+        """検波した読み出しパルス波形表示"""
+        avg_num = 50  # 平均化する個数
 
-    axs[1].scatter(rotated_states.real, rotated_states.imag)
-    axs[1].set_title("After Rotation")
+        mov_avg_readout_iq = (
+            np.convolve(readout_rx_waveforms[qubit], np.ones(avg_num), mode="valid")
+            / avg_num
+        )  # 移動平均
+        mov_avg_readout_iq = np.append(mov_avg_readout_iq, np.zeros(avg_num - 1))
 
-    for ax in axs:
-        ax.axis("equal")
-        ax.set_xlabel("I")
-        ax.set_ylabel("Q")
-        ax.grid(True)
+        ax[qubit][0].plot(
+            readout_rx_times[qubit] * 1e-3,
+            np.real(mov_avg_readout_iq),
+            label="I",
+        )
+        ax[qubit][0].plot(
+            readout_rx_times[qubit] * 1e-3,
+            np.imag(mov_avg_readout_iq),
+            label="Q",
+        )
 
-    plt.show()
+        ax[qubit][0].plot(
+            readout_rx_times[qubit][readout_range] * 1e-3,
+            np.real(mov_avg_readout_iq)[readout_range],
+            lw=5,
+        )
+        ax[qubit][0].plot(
+            readout_rx_times[qubit][readout_range] * 1e-3,
+            np.imag(mov_avg_readout_iq)[readout_range],
+            lw=5,
+        )
 
+        ax[qubit][0].set_xlabel("Time (μs)")
+        ax[qubit][0].set_xlim(0, 2.0)
+        ax[qubit][0].set_title("Detected readout pulse waveform " + qubit)
+        ax[qubit][0].legend()
+        ax[qubit][0].grid()
 
-def plot_states_vs_index(data: npt.ArrayLike):
-    states = np.array(data)
-    _, ax = plt.subplots(figsize=(8, 4))
+        """Rabi振動"""
+        ax[qubit][1].plot(sweep_range, np.real(signals[qubit]), "o-", label="I")
+        ax[qubit][1].plot(sweep_range, np.imag(signals[qubit]), "o-", label="Q")
+        ax[qubit][1].set_xlabel("Sweep index")
+        ax[qubit][1].set_title("Detected signal " + qubit)
+        ax[qubit][1].legend()
+        ax[qubit][1].grid()
 
-    ax.plot(states.real)
-    ax.plot(states.imag)
-    ax.set_xlabel("Index")
-    ax.set_ylabel("Amplitude / a.u.")
-    ax.grid(True)
+        """IQ平面上での複素振幅"""
+        ax[qubit][2].plot(
+            np.real(mov_avg_readout_iq), np.imag(mov_avg_readout_iq), lw=0.2
+        )
 
-    plt.show()
+        width = max(np.abs(signals[qubit]))
+        ax[qubit][2].set_xlim(-width, width)
+        ax[qubit][2].set_ylim(-width, width)
+        ax[qubit][2].plot(
+            np.linspace(-width, width, 2), np.zeros(2), linewidth=1, color="black"
+        )
+        ax[qubit][2].plot(
+            np.zeros(2), np.linspace(-width, width, 2), linewidth=1, color="black"
+        )
+        ax[qubit][2].set_xlabel("I")
+        ax[qubit][2].set_ylabel("Q")
+        ax[qubit][2].set_title("Complex amplitude on IQ plane " + qubit)
 
+        ax[qubit][2].scatter(
+            np.real(signals[qubit]),
+            np.imag(signals[qubit]),
+            label="Before rotation",
+        )
+        ax[qubit][2].scatter(
+            np.real(signals[qubit])[0],
+            np.imag(signals[qubit])[0],
+            color="blue",
+        )
 
-def plot_states_vs_times(times: npt.ArrayLike, data: npt.ArrayLike):
-    states = np.array(data)
-    _, ax = plt.subplots(figsize=(8, 4))
-
-    ax.plot(times, states.real)
-    ax.plot(times, states.imag)
-    ax.set_xlabel("Time / ns")
-    ax.set_ylabel("Amplitude / a.u.")
-    ax.grid(True)
-
+        ax[qubit][2].scatter(
+            np.real(signals_rotated[qubit]),
+            np.imag(signals_rotated[qubit]),
+            label="After rotation",
+        )
+        ax[qubit][2].scatter(
+            np.real(signals_rotated[qubit][0]),
+            np.imag(signals_rotated[qubit][0]),
+            color="red",
+        )
+        ax[qubit][2].legend()
+        ax[qubit][2].grid()
     plt.show()
