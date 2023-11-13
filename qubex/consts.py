2852801eccd2aede1cff5067a4718fb3549a9d83
Akinori Machino <@amachino>
2023-11-13 16:57:32 +0900

    Add Final keyword to consts.py variables

--- qubex/consts.py
+++ qubex/consts.py
@@ -1,7 +1,9 @@
-SAMPLING_PERIOD: int = 2  # [ns]
-MIN_SAMPLE: int = 64  # min number of samples of e7awg
-MIN_DURATION: int = MIN_SAMPLE * SAMPLING_PERIOD
-T_CONTROL: int = 10 * 1024  # [ns]
-T_READOUT: int = 1024  # [ns]
-T_MARGIN: int = MIN_DURATION  # [ns]
-MUX = [[f"Q{i*4+j:02d}" for j in range(4)] for i in range(16)]
+from typing import Final
+
+SAMPLING_PERIOD: Final[int] = 2  # [ns]
+MIN_SAMPLE: Final[int] = 64  # min number of samples of e7awg
+MIN_DURATION: Final[int] = MIN_SAMPLE * SAMPLING_PERIOD
+T_CONTROL: Final[int] = 10 * 1024  # [ns]
+T_READOUT: Final[int] = 1024  # [ns]
+T_MARGIN: Final[int] = MIN_DURATION  # [ns]
+MUX: Final[list[list[str]]] = [[f"Q{i*4+j:02d}" for j in range(4)] for i in range(16)]
