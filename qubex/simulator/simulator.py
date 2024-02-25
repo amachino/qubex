Fix dynamic Hamiltonian calculation

qubex/simulator/simulator.py
@@ -148,8 +148,8 @@ class Simulator:
             static_hamiltonian -= 2 * np.pi * control.frequency * ad * a
 
             if transmon.label == control.target:
-                dynamic_hamiltonian.append([0.5 * a, control.values])
-                dynamic_hamiltonian.append([0.5 * ad, np.conj(control.values)])
+                dynamic_hamiltonian.append([0.5 * ad, control.values])
+                dynamic_hamiltonian.append([0.5 * a, np.conj(control.values)])
 
             decay_operator = np.sqrt(transmon.decay_rate) * a
             dephasing_operator = np.sqrt(transmon.dephasing_rate) * ad * a