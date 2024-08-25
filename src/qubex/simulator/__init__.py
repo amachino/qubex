76bd2e4c9e706acb2a3d46467fdf2cd799fe3640
Akinori Machino <82702+amachino@users.noreply.github.com>
2024-08-25 12:52:10 +0900

    refactor: Update qubex simulator module imports

--- src/qubex/simulator/__init__.py
+++ src/qubex/simulator/__init__.py
@@ -1,11 +1,13 @@
-from .simulator import Control, MultiControl, Simulator
-from .system import Coupling, System, Transmon
+from .quantum_simulator import Control, MultiControl, QuantumSimulator
+from .quantum_system import Coupling, QuantumSystem, Qubit, Resonator, Transmon
 
 __all__ = [
     "Control",
     "Coupling",
     "MultiControl",
-    "Simulator",
-    "System",
+    "QuantumSimulator",
+    "QuantumSystem",
+    "Qubit",
+    "Resonator",
     "Transmon",
 ]
