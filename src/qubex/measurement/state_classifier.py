e88ff2435d7c3c27963d4091cc73dc94a0409b32
Akinori Machino <@amachino>
2024-09-04 21:44:10 +0900

    refactor: Update state_classifier.py to use StateClassifierKMeans instead of StateClassifierGMM

--- src/qubex/measurement/state_classifier.py
+++ src/qubex/measurement/state_classifier.py
@@ -1,6 +1,5 @@
-from .state_classifier_gmm import StateClassifierGMM as StateClassifier
-
-# from .state_classifier_kmeans import StateClassifierKMeans as StateClassifier
+# from .state_classifier_gmm import StateClassifierGMM as StateClassifier
+from .state_classifier_kmeans import StateClassifierKMeans as StateClassifier
 
 __all__ = [
     "StateClassifier",
