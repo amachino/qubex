2f39a1ccdc9c2a7ebe5063034355ef8cf9f970e9
Akinori Machino <@amachino>
2025-07-02 15:56:55 +0900

    refactor: streamline import statements and logging in __init__.py

--- src/qubex/__init__.py
+++ src/qubex/__init__.py
@@ -1,30 +1,19 @@
 import logging
 
+from . import pulse
+from .analysis import fitting as fit
+from .analysis import visualization as viz
+from .experiment import Experiment
+from .pulse import Blank, Pulse, PulseArray, PulseChannel, PulseSchedule, VirtualZ
 from .style import apply_template
 
+apply_template("qubex")
 logger = logging.getLogger(__name__)
 
 try:
     import qubex.patches.quel_ic_config.abstract_nco_ftw_patch  # noqa: F401
 except ImportError:
-    logger.info("Failed to import abstract_nco_ftw_patch.")
-
-try:
-    from .experiment import Experiment
-except ImportError:
-    logger.info("Failed to import Experiment.")
-    pass
-
-try:
-    from . import pulse
-    from .analysis import fitting as fit
-    from .analysis import visualization as viz
-    from .pulse import Blank, Pulse, PulseArray, PulseChannel, PulseSchedule, VirtualZ
-except ImportError as e:
-    logger.error(f"Import error: {e}")
-
-apply_template("qubex")
-
+    logger.debug("Failed to import abstract_nco_ftw_patch.")
 
 __all__ = [
     "fit",
