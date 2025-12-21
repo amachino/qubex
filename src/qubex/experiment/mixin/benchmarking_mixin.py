8cb4df5f44c851dd6a015483240ca06f913268e9
Bannnnnnnnnn <k.s.ban0427@gmail.com>
2025-12-21 22:15:54 +0900

    add stark rb 1q

--- src/qubex/experiment/mixin/benchmarking_mixin.py
+++ src/qubex/experiment/mixin/benchmarking_mixin.py
@@ -11,7 +11,7 @@ from ...analysis import visualization as viz
 from ...backend import Target
 from ...clifford import Clifford
 from ...measurement.measurement import DEFAULT_INTERVAL, DEFAULT_SHOTS
-from ...pulse import PulseArray, PulseSchedule, VirtualZ, Waveform
+from ...pulse import Blank, FlatTop, PulseArray, PulseSchedule, VirtualZ, Waveform
 from ...typing import TargetMap
 from ..protocol import BaseProtocol, BenchmarkingProtocol, MeasurementProtocol
 from ..result import Result
@@ -1017,3 +1017,418 @@ class BenchmarkingMixin(
                 except Exception as e:
                     print(f"Failed to benchmark {target}: {e}")
                     continue
+
+    def stark_rb_experiment_1q(
+        self,
+        target: str,
+        *,
+        stark_amplitude: float,
+        stark_ramptime: int,
+        n_cliffords_range: ArrayLike | None = None,
+        n_trials: int | None = None,
+        seeds: ArrayLike | None = None,
+        max_n_cliffords: int | None = None,
+        x90: TargetMap[Waveform] | None = None,
+        interleaved_clifford: Clifford | None = None,
+        interleaved_waveform: TargetMap[Waveform] | None = None,
+        shots: int | None = None,
+        interval: float | None = None,
+        xaxis_type: Literal["linear", "log"] | None = None,
+        plot: bool = True,
+        save_image: bool = True,
+    ) -> Result:
+        st = self.stark_target(target=target)
+        ins = self.insitu_target(target=target)
+
+        stark_ampl = self.calc_control_amplitude(
+            target=target, rabi_rate=stark_amplitude
+        )
+
+        if n_cliffords_range is not None:
+            n_cliffords_range = np.array(n_cliffords_range, dtype=int)
+
+        if n_trials is None:
+            n_trials = DEFAULT_RB_N_TRIALS
+
+        if seeds is None:
+            seeds = np.random.randint(0, 2**32, n_trials)
+        else:
+            seeds = np.array(seeds, dtype=int)
+            if len(seeds) != n_trials:
+                raise ValueError(
+                    "The number of seeds must be equal to the number of trials."
+                )
+
+        if max_n_cliffords is None:
+            max_n_cliffords = DEFAULT_MAX_N_CLIFFORDS_1Q
+
+        if shots is None:
+            shots = DEFAULT_SHOTS
+
+        if interval is None:
+            interval = DEFAULT_INTERVAL
+
+        if xaxis_type is None:
+            xaxis_type = "linear"
+
+        target_object = self.experiment_system.get_target(target)
+        if target_object.is_cr:
+            raise ValueError(f"`{target}` is not a 1Q target.")
+
+        def rb_sequence(
+            target: str,
+            n_clifford: int,
+            seed: int,
+        ) -> PulseSchedule:
+            with PulseSchedule() as ps:
+                rb_sequence = self.rb_sequence_1q(
+                    target,
+                    n=n_clifford,
+                    x90=x90.get(target) if x90 else None,
+                    interleaved_waveform=interleaved_waveform.get(target)
+                    if interleaved_waveform
+                    else None,
+                    interleaved_clifford=interleaved_clifford,
+                    seed=seed,
+                )
+                stark_pulse = FlatTop(
+                    duration=rb_sequence.duration + stark_ramptime * 2,
+                    amplitude=stark_ampl,
+                    tau=stark_ramptime,
+                )
+                ps.add(st, stark_pulse)
+                ps.add(ins, Blank(stark_ramptime))
+                ps.add(ins, rb_sequence)
+            return ps
+
+        return_data = {}
+
+        idx = 0
+        sweep_range = []
+        mean_data = defaultdict(list)
+        std_data = defaultdict(list)
+        while True:
+            if n_cliffords_range is None:
+                n_clifford = 0 if idx == 0 else 2 ** (idx - 1)
+                if n_clifford > max_n_cliffords:
+                    break
+            else:
+                if idx >= len(n_cliffords_range):
+                    break
+                n_clifford = n_cliffords_range[idx]
+
+            idx += 1
+            sweep_range.append(n_clifford)
+
+            trial_data = defaultdict(list)
+            for seed in seeds:
+                seed = int(seed)  # Ensure seed is an integer
+                result = self.measure(
+                    sequence=rb_sequence(
+                        n_clifford=n_clifford,
+                        target=target,
+                        seed=seed,
+                    ),
+                    mode="avg",
+                    shots=shots,
+                    interval=interval,
+                    plot=False,
+                )
+                for target, data in result.data.items():
+                    iq = data.kerneled
+                    z = self.rabi_params[target].normalize(iq)
+                    trial_data[target].append((z + 1) / 2)
+
+            check_vals = {}
+
+            mean = np.mean(trial_data[target])
+            std = np.std(trial_data[target])
+            mean_data[target].append(mean)
+            std_data[target].append(std)
+            check_vals[target] = mean - std * 0.5
+
+            max_check_val = np.max(list(check_vals.values()))
+            if n_cliffords_range is None and max_check_val < 0.5:
+                break
+
+        sweep_range = np.array(sweep_range, dtype=int)
+
+        mean_data = {target: np.array(data) for target, data in mean_data.items()}
+        std_data = {target: np.array(data) for target, data in std_data.items()}
+
+        mean = mean_data[target]
+        std = std_data[target] if n_trials > 1 else None
+
+        fit_result = fitting.fit_rb(
+            target=target,
+            x=sweep_range,
+            y=mean,
+            error_y=std,
+            bounds=((0, 0, 0), (0.5, 1, 1)),
+            title="Randomized benchmarking",
+            xlabel="Number of Cliffords",
+            ylabel="Normalized signal",
+            xaxis_type=xaxis_type,
+            yaxis_type="linear",
+            plot=plot,
+        )
+
+        if save_image:
+            viz.save_figure_image(
+                fit_result["fig"],
+                name=f"rb_experiment_1q_{target}",
+            )
+
+        return_data[target] = {
+            "n_cliffords": sweep_range,
+            "mean": mean,
+            "std": std,
+            **fit_result,
+        }
+
+        return Result(data=return_data)
+
+    def stark_irb_experiment(
+        self,
+        target: str,
+        *,
+        stark_amplitude: float,
+        stark_ramptime: int,
+        interleaved_clifford: str | Clifford,
+        interleaved_waveform: TargetMap[PulseSchedule]
+        | TargetMap[Waveform]
+        | None = None,
+        n_cliffords_range: ArrayLike | None = None,
+        n_trials: int | None = None,
+        seeds: ArrayLike | None = None,
+        max_n_cliffords: int | None = None,
+        x90: TargetMap[Waveform] | None = None,
+        zx90: TargetMap[PulseSchedule] | None = None,
+        shots: int | None = None,
+        interval: float | None = None,
+        plot: bool = True,
+        save_image: bool = True,
+    ) -> Result:
+        if isinstance(interleaved_clifford, str):
+            clifford = self.clifford.get(interleaved_clifford)
+            if clifford is None:
+                raise ValueError(f"Invalid Clifford: {interleaved_clifford}")
+            interleaved_clifford = clifford
+
+        is_2q = self.experiment_system.get_target(target[0]).is_cr
+
+        if is_2q:
+            dimension = 4
+            rb_result = self.rb_experiment_2q(
+                target,
+                n_cliffords_range=n_cliffords_range,
+                n_trials=n_trials,
+                seeds=seeds,
+                max_n_cliffords=max_n_cliffords,
+                x90=x90,
+                zx90=zx90,
+                in_parallel=False,
+                shots=shots,
+                interval=interval,
+                plot=False,
+                save_image=False,
+            )
+            irb_result = self.rb_experiment_2q(
+                targets=target,
+                n_cliffords_range=n_cliffords_range,
+                n_trials=n_trials,
+                seeds=seeds,
+                max_n_cliffords=max_n_cliffords,
+                x90=x90,
+                zx90=zx90,
+                interleaved_waveform=interleaved_waveform,  # type: ignore
+                interleaved_clifford=interleaved_clifford,
+                in_parallel=False,
+                shots=shots,
+                interval=interval,
+                plot=False,
+                save_image=False,
+            )
+        else:
+            dimension = 2
+            rb_result = self.stark_rb_experiment_1q(
+                target,
+                stark_amplitude=stark_amplitude,
+                stark_ramptime=stark_ramptime,
+                n_cliffords_range=n_cliffords_range,
+                n_trials=n_trials,
+                seeds=seeds,
+                max_n_cliffords=max_n_cliffords,
+                x90=x90,
+                shots=shots,
+                interval=interval,
+                plot=False,
+                save_image=False,
+            )
+            irb_result = self.stark_rb_experiment_1q(
+                targets=target,
+                stark_amplitude=stark_amplitude,
+                stark_ramptime=stark_ramptime,
+                n_cliffords_range=n_cliffords_range,
+                n_trials=n_trials,
+                seeds=seeds,
+                max_n_cliffords=max_n_cliffords,
+                x90=x90,
+                interleaved_waveform=interleaved_waveform,  # type: ignore
+                interleaved_clifford=interleaved_clifford,
+                shots=shots,
+                interval=interval,
+                plot=False,
+                save_image=False,
+            )
+
+        results = {}
+        rb_n_cliffords = rb_result[target]["n_cliffords"]
+        rb_mean = rb_result[target]["mean"]
+        rb_std = rb_result[target]["std"]
+        rb_fit_result = fitting.fit_rb(
+            target=target,
+            x=rb_n_cliffords,
+            y=rb_mean,
+            error_y=rb_std,
+            dimension=dimension,
+            plot=False,
+        )
+        A_rb = rb_fit_result["A"]
+        p_rb = rb_fit_result["p"]
+        p_rb_err = rb_fit_result["p_err"]
+        C_rb = rb_fit_result["C"]
+        avg_gate_error_rb = rb_fit_result["avg_gate_error"]
+        avg_gate_fidelity_rb = rb_fit_result["avg_gate_fidelity"]
+        avg_gate_fidelity_err_rb = rb_fit_result["avg_gate_fidelity_err"]
+
+        irb_n_cliffords = irb_result[target]["n_cliffords"]
+        irb_mean = irb_result[target]["mean"]
+        irb_std = irb_result[target]["std"]
+        irb_fit_result = fitting.fit_rb(
+            target=target,
+            x=irb_n_cliffords,
+            y=irb_mean,
+            error_y=irb_std,
+            dimension=dimension,
+            plot=False,
+            title="Interleaved randomized benchmarking",
+        )
+        A_irb = irb_fit_result["A"]
+        p_irb = irb_fit_result["p"]
+        p_irb_err = irb_fit_result["p_err"]
+        C_irb = irb_fit_result["C"]
+        avg_gate_fidelity_irb = irb_fit_result["avg_gate_fidelity"]
+        avg_gate_fidelity_err_irb = irb_fit_result["avg_gate_fidelity_err"]
+
+        gate_error = (dimension - 1) * (1 - (p_irb / p_rb)) / dimension
+        gate_fidelity = 1 - gate_error
+
+        gate_fidelity_err = (
+            (dimension - 1)
+            / dimension
+            * np.sqrt((p_irb_err / p_rb) ** 2 + (p_rb_err * p_irb / p_rb**2) ** 2)
+        )
+
+        fig = fitting.plot_irb(
+            target=target,
+            x=rb_n_cliffords,
+            y_rb=rb_mean,
+            y_irb=irb_mean,
+            error_y_rb=rb_std,
+            error_y_irb=irb_std,
+            A_rb=A_rb,
+            A_irb=A_irb,
+            p_rb=p_rb,
+            p_irb=p_irb,
+            C_rb=C_rb,
+            C_irb=C_irb,
+            gate_fidelity=gate_fidelity,
+            gate_fidelity_err=gate_fidelity_err,
+            plot=plot,
+            title=f"Interleaved randomized benchmarking of {clifford.name}",
+            xlabel="Number of Cliffords",
+            ylabel="Normalized signal",
+        )
+        if save_image:
+            viz.save_figure_image(
+                fig,
+                name=f"interleaved_randomized_benchmarking_{target}",
+            )
+
+        print()
+        print(
+            f"Average gate fidelity (RB)  : {avg_gate_fidelity_rb * 100:.3f} ± {avg_gate_fidelity_err_rb * 100:.3f}%"
+        )
+        print(
+            f"Average gate fidelity (IRB) : {avg_gate_fidelity_irb * 100:.3f} ± {avg_gate_fidelity_err_irb * 100:.3f}%"
+        )
+        print()
+        print(
+            f"Gate error    : {gate_error * 100:.3f} ± {gate_fidelity_err * 100:.3f}%"
+        )
+        print(
+            f"Gate fidelity : {gate_fidelity * 100:.3f} ± {gate_fidelity_err * 100:.3f}%"
+        )
+        print()
+
+        if gate_error < 0.1 * avg_gate_error_rb:
+            # TODO: use a more appropriate threshold based on the system.
+            # NOTE: average number of gates per 2Q Clifford: 1Q=2.589, 2Q=1.5
+            print(
+                f"Warning: Gate error ({gate_error * 100:.3f}%) is too low compared to the average gate error (RB) ({avg_gate_error_rb * 100:.3f}%)."
+            )
+
+        results[target] = {
+            "gate_error": gate_error,
+            "gate_fidelity": gate_fidelity,
+            "gate_fidelity_err": gate_fidelity_err,
+            "rb_fit_result": rb_fit_result,
+            "irb_fit_result": irb_fit_result,
+            "fig": fig,
+        }
+        return Result(data=results)
+
+    def stark_interleaved_randomized_benchmarking(
+        self,
+        target: str,
+        *,
+        stark_amplitude: float,
+        stark_ramptime: int,
+        interleaved_clifford: str | Clifford,
+        interleaved_waveform: TargetMap[PulseSchedule]
+        | TargetMap[Waveform]
+        | None = None,
+        n_cliffords_range: ArrayLike | None = None,
+        n_trials: int | None = None,
+        seeds: ArrayLike | None = None,
+        max_n_cliffords: int | None = None,
+        x90: TargetMap[Waveform] | None = None,
+        zx90: TargetMap[PulseSchedule] | None = None,
+        shots: int | None = None,
+        interval: float | None = None,
+        plot: bool = True,
+        save_image: bool = True,
+    ) -> Result:
+        results = {}
+        result = self.stark_irb_experiment(
+            targets=target,
+            stark_amplitude=stark_amplitude,
+            stark_ramptime=stark_ramptime,
+            interleaved_clifford=interleaved_clifford,
+            interleaved_waveform=interleaved_waveform,
+            n_cliffords_range=n_cliffords_range,
+            n_trials=n_trials,
+            seeds=seeds,
+            max_n_cliffords=max_n_cliffords,
+            x90=x90,
+            zx90=zx90,
+            shots=shots,
+            interval=interval,
+            plot=plot,
+            save_image=save_image,
+        )
+        results[target] = result[target]
+        data = Result(data=results)
+
+        return Result(data=data.data)
