83351dcd104dbc3c2bf729d5a9fd7d34fec5a511
Matsuda91 <matsuda.klab@gmail.com>
2026-03-20 00:21:13 +0900

    feat: implement Rabi population characterization and normalize data for thermal excitation

--- src/qubex/contrib/experiment/characterize_thermal_excitation.py
+++ src/qubex/contrib/experiment/characterize_thermal_excitation.py
@@ -1,37 +1,87 @@
 import numpy as np
-from qxpulse import PulseSchedule
+import qxvisualizer as viz
+from qxpulse import FlatTop, PulseSchedule
 
+import qubex.analysis.fitting as fitting
 from qubex import Experiment
 from qubex.experiment.experiment_result import ExperimentResult, SweepData
+from qubex.system.target import Target
 
 
 def characterize_thermal_excitation_via_rabi(
     exp: Experiment,
     *,
-    targets: list[str],
+    target: str,
     time_range: np.ndarray,
 ) -> float:
 
-    def _sequence_1(T: int) -> PulseSchedule:
-        pass
+    def _sequence_g_population_rabi(T: int) -> PulseSchedule:
+        ef_label = Target.ef_label(target)
 
-    def _sequence_2(T: int) -> PulseSchedule:
-        pass
+        with PulseSchedule() as ps:
+            ps.add(target, exp.x180(target))
+            ps.barrier()
+            ps.add(ef_label, FlatTop(duration=T))
+            ps.barrier()
+            ps.add(target, exp.x180(target))
+        return ps
 
-    result_1: ExperimentResult[SweepData] = exp.sweep_parameter(
-        sequence=_sequence_1,
+    def _sequence_e_population_rabi(T: int) -> PulseSchedule:
+        ef_label = Target.ef_label(target)
+
+        with PulseSchedule() as ps:
+            ps.add(ef_label, FlatTop(duration=T))
+            ps.barrier()
+            ps.add(target, exp.x180(target))
+        return ps
+
+    result_g: ExperimentResult[SweepData] = exp.sweep_parameter(
+        sequence=_sequence_g_population_rabi,
         sweep_range=time_range,
         plot=False,
     )
 
-    result_2: ExperimentResult[SweepData] = exp.sweep_parameter(
-        sequence=_sequence_2,
+    result_e: ExperimentResult[SweepData] = exp.sweep_parameter(
+        sequence=_sequence_e_population_rabi,
         sweep_range=time_range,
         plot=False,
     )
 
-    result_1.plot()
-    result_2.plot()
+    state_centers = exp.state_centers[target]
+    if state_centers is None:
+        raise ValueError(f"State centers for target {target} are not defined.")
+    if len(state_centers) < 3:
+        raise ValueError(
+            f"State centers for target {target} should have at least 3 states (g, e, f)."
+        )
+
+    def normalized(data: np.ndarray[np.complex128]) -> np.ndarray:
+        c_g = state_centers[0]
+        c_f = state_centers[2]
+
+        v_gf = c_f - c_g
+        v_gd = data - c_g
+        return np.real(v_gd * np.conj(v_gf)) / np.abs(v_gf)
+
+    data_g = normalized(result_g.data[target].data)
+    data_e = normalized(result_e.data[target].data)
+
+    fit_result_g: fitting.FitResult = fitting.fit_rabi()
+    fit_result_e: fitting.FitResult = fitting.fit_rabi()
+
+    fig = viz.make_figure()
+    for d in fit_result_g.data["fig"]:
+        for trace in d.data:
+            fig.add_trace(trace)
+    for d in fit_result_e.data["fig"]:
+        for trace in d.data:
+            fig.add_trace(trace)
+    fig.show()
+
+    print("")
+    print(f"target : {target}")
+    print("p_ex: xx")
+    print("")
 
 
 def characterize_thermal_excitation_via_Gaussian_fit(
