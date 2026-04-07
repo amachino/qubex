497a7cf813aa0da23c9e645c395c5fc879622877
Matsuda91 <matsuda.klab@gmail.com>
2026-04-07 13:11:36 +0900

    feat: refine characterize_thermal_excitation_via_rabi with improved parameter handling and plotting

--- src/qubex/contrib/experiment/characterize_thermal_excitation.py
+++ src/qubex/contrib/experiment/characterize_thermal_excitation.py
@@ -3,13 +3,14 @@ from __future__ import annotations
 from collections import defaultdict
 
 import numpy as np
-import plotly.graph_objects as go
-import qxvisualizer as viz
 from qxpulse import FlatTop, PulseSchedule
+from tqdm import tqdm
 
 import qubex.analysis.fitting as fitting
 from qubex import Experiment
+from qubex.experiment.experiment_constants import DEFAULT_RABI_TIME_RANGE
 from qubex.experiment.experiment_result import ExperimentResult, SweepData
+from qubex.experiment.result import Result
 from qubex.pulse import VirtualZ
 from qubex.system.target import Target
 
@@ -18,23 +19,34 @@ def characterize_thermal_excitation_via_rabi(
     exp: Experiment,
     *,
     target: str,
-    time_range: np.ndarray,
+    time_range: np.ndarray | None = None,
+    theta_range: np.ndarray | None = None,
     ramptime: float | None = None,
-    obtain_reference_gf: bool = True,
+    ef_rabi_amplitude: float | None = None,
+    plot: bool = False,
 ) -> float:
+
+    if time_range is None:
+        time_range = DEFAULT_RABI_TIME_RANGE
+    if theta_range is None:
+        theta_range = np.linspace(0, 1.5 * np.pi, 21)
 
     if ramptime is None:
         ramptime = 0
 
-    effective_time_range = time_range + ramptime
     reference_points = exp.obtain_reference_points(
         target,
     )
+    if ef_rabi_amplitude is None:
+        ef_rabi_amplitude = exp.params.control_amplitude[target] / np.sqrt(2)
 
-    theta_list = np.linspace(0, np.pi, 11)
+    time_range = np.asarray(time_range)
+    effective_time_range = time_range + ramptime
 
-    fit_rabi_amplitudes = defaultdict(list)
-    for theta in theta_list:
+    fit_theta_history = defaultdict(list)
+    fit_rabi_amplitude_history = defaultdict(list)
+    result_history = []
+    for theta in tqdm(theta_range):
 
         def _sequence_population_rabi(
             T: int,
@@ -49,8 +61,8 @@ def characterize_thermal_excitation_via_rabi(
                 ps.add(
                     ef_label,
                     FlatTop(
-                        duration=T,
-                        amplitude=exp.params.control_amplitude[target] / np.sqrt(2),
+                        duration=T + 2 * ramptime,
+                        amplitude=ef_rabi_amplitude,
                         tau=ramptime,
                     ),
                 )
@@ -61,40 +73,82 @@ def characterize_thermal_excitation_via_rabi(
         result: ExperimentResult[SweepData] = exp.sweep_parameter(
             sequence=_sequence_population_rabi,
             sweep_range=time_range,
-            plot=True,
+            plot=plot,
         )
+        result_history.append(result)
 
         for target, data in result.data.items():
-            fit_result = fitting.fit_rabi(
+            fit_rabi_result = fitting.fit_rabi(
                 target=target,
                 times=effective_time_range,
                 data=result.data[target].data,
                 reference_point=reference_points.data["iq"][target],
-                plot=True,
+                plot=plot,
             )
-            fit_rabi_amplitudes[target].append(fit_result.data["amplitude"])
+            r2 = fit_rabi_result.data["r2"]
+            if r2 >= 0.9:
+                fit_rabi_amplitude_history[target].append(
+                    fit_rabi_result.data["amplitude"]
+                )
+                fit_theta_history[target].append(theta)
 
-    fig = viz.make_figure()
-
-    fig.add_trace(
-        go.Scatter(
-            x=theta_list,
-            y=fit_rabi_amplitudes[target],
-            mode="markers+lines",
-            name="Rabi amplitude",
+    fit_cosine_result = defaultdict(list)
+    for target in fit_theta_history.keys():
+        fit_cosine_result[target] = fitting.fit_cosine(
+            x=fit_theta_history[target],
+            y=fit_rabi_amplitude_history[target],
+            plot=False,
         )
+
+        popt = fit_cosine_result[target].data["popt"]
+        rabi_ampl_0 = fitting.func_cos(0, *popt)
+        rabi_ampl_pi = fitting.func_cos(np.pi, *popt)
+        p_ex = rabi_ampl_0 / (rabi_ampl_0 + rabi_ampl_pi)
+        ef_rabi_freq = exp.calc_control_amplitude(target, ef_rabi_amplitude)
+
+        fig = fit_cosine_result[target].data["fig"]
+        fig.update_layout(
+            title=dict(
+                text=f"Thermal excitation characterization via Rabi - {target}",
+                subtitle=dict(
+                    text=f"Ωef = {ef_rabi_freq * 1e3:.1f} MHz, p_ex = {p_ex:.4f}"
+                ),
+            ),
+            xaxis_title="Theta (rad)",
+            yaxis_title="Rabi Amplitude (a.u.)",
+        )
+
+        fig.show()
+
+        A = fit_cosine_result[target].data["A"]
+        A_err = fit_cosine_result[target].data["A_err"]
+        f = fit_cosine_result[target].data["f"]
+        f_err = fit_cosine_result[target].data["f_err"]
+        phi = fit_cosine_result[target].data["phi"]
+        phi_err = fit_cosine_result[target].data["phi_err"]
+        C = fit_cosine_result[target].data["C"]
+        C_err = fit_cosine_result[target].data["C_err"]
+
+        print("")
+        print(f"target : {target}")
+        print(f"A   : {A} ± {A_err}")
+        print(f"f   : {f} ± {f_err}")
+        print(f"phi : {phi} ± {phi_err}")
+        print(f"C   : {C} ± {C_err}")
+        print("")
+        print("thermal excitation probability (p_ex):")
+        print(f"rabi amplitude at θ=0 : {rabi_ampl_0:.4f}")
+        print(f"rabi amplitude at θ=π : {rabi_ampl_pi:.4f}")
+        print(f"p_ex : {p_ex:.4f}")
+        print("")
+    return Result(
+        data={
+            "time_range": time_range,
+            "theta_range": theta_range,
+            "result_history": result_history,
+        },
+        figure=fig,
     )
-    fig.update_xaxes(
-        title_text="θ (rad)",
-    )
-    fig.update_yaxes(
-        title_text="Rabi amplitude (a.u.)",
-    )
-    fig.show()
-    print("")
-    print(f"target : {target}")
-    print("p_ex: xx")
-    print("")
 
 
 def characterize_thermal_excitation_via_Gaussian_fit(
