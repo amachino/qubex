682a34b047ea3c5e14c33934f0a9de3cbc58a828
András Márton Gunyhó <andras.gunyho@riken.jp>
2026-07-24 16:23:22 +0900

    Remove Quel1-SE R8 specific information from error message

--- src/qubex/system/quel1/quel1_system_synchronizer.py
+++ src/qubex/system/quel1/quel1_system_synchronizer.py
@@ -276,13 +276,15 @@ class Quel1SystemSynchronizer:
                 ):
                     logger.warning(
                         "Skipping backend port sync for %s:%s due to fnco count mismatch "
-                        "(expected=%s, actual=%s). Verify that the AWG profile in the "
-                        "box.yaml is correct (try se8_mxfe1_awg1331, se8_mxfe1_awg2222, "
-                        "or se8_mxfe1_awg3113) - see https://amachino.github.io/qubex/user-guide/getting-started/system-configuration/#boxyaml",
+                        "(expected=%s, actual=%s). Verify that the 'type' and 'options' "
+                        "entries of %s in box.yaml match its active hardware "
+                        "configuration. For further details, see "
+                        "https://amachino.github.io/qubex/user-guide/getting-started/system-configuration/#boxyaml",
                         box_id,
                         port_number,
                         expected_fnco_count,
                         len(fnco_freqs_hz),
+                        box_id,
                     )
                     continue
                 updates.append(
